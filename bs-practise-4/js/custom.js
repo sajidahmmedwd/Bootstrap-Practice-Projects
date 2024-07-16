ba363dda1cd4629c734718d70bc67eb88a3196f3
Sajid Ahmmed <81010996+sajidahmmedwd@users.noreply.github.com>
2024-07-16 12:57:45 +0600

    Add files via upload

---
+++ bs-practise-4/js/custom.js
@@ -0,0 +1,14 @@
+// -------- type.js -------
+
+	var typed = new Typed('.animate', {
+  strings: [
+  'Web Designer', 
+  'Web Developer'
+  'Freelancer'
+
+  ],
+
+  typeSpeed: 100,
+  backSpeed: 100,
+  loop: true,
+});
